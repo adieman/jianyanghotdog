e4d9d415117bd1cee309ff0aa9d5cab9ca94c340
adieman <amatorio.vladimir@gmail.com>
2019-10-05 21:33:45 +0800

    Third Commit, document undefined

--- js_script.js
+++ js_script.js
@@ -1,8 +1,8 @@
 function userClicked() {
-    console.log("Someone Clicked the button")
+    console.log('Someone Clicked the button');
 }
 
-document.getElementById("btn").addEventListener("click", doStuff);
+document.getElementById('btn').addEventListener("click", doStuff);
 
 function doStuff() {
     return userClicked;
