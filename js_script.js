bddd8824412baa8e51e8dea06be12f52761e3f0c
adieman <amatorio.vladimir@gmail.com>
2019-10-05 20:41:47 +0800

    Testing JS and HTML

---
+++ js_script.js
@@ -0,0 +1,9 @@
+function userClicked() {
+    console.log("Someone Clicked the button")
+}
+
+document.getElementById("btn").addEventListener("click", doStuff);
+
+function doStuff() {
+    return userClicked;
+}
